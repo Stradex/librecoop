3682008c1ad3fd27973b43a9b7a3a93a2268d625
Stradex <stradexengine@gmail.com>
2019-12-17 00:16:13 -0300

    Crash fix ported to D3XP

--- d3xp/physics/Physics_AF.cpp
+++ d3xp/physics/Physics_AF.cpp
@@ -5419,6 +5419,15 @@ bool idPhysics_AF::CollisionImpulse( float timeStep, idAFBody *body, trace_t &co
 		return false;
 	}
 
+	//Stradex: you know that at this point this shitty mod is full of duct-tape, don't you?
+	if (gameLocal.mpGame.IsGametypeCoopBased() && (FLOAT_IS_NAN(collision.endpos.x) || FLOAT_IS_NAN(collision.endpos.y) || FLOAT_IS_NAN(collision.endpos.z) ||
+		FLOAT_IS_NAN(collision.c.normal.x) || FLOAT_IS_NAN(collision.c.normal.y) || FLOAT_IS_NAN(collision.c.normal.z) ||
+		FLOAT_IS_NAN(collision.c.normal.x) ||  FLOAT_IS_NAN(collision.c.normal.y) ||  FLOAT_IS_NAN(collision.c.normal.z)) ) {
+		common->Warning("[COOP FATAL] NAN Float at idPhysics_AF::CollisionImpulse\n");
+		return false;
+	}
+
+
 	// get info from other entity involved
 	ent->GetImpactInfo( self, collision.c.id, collision.c.point, &info );
 	// collision point relative to the body center of mass
